a755af907b9b8b9b66ebc849570406954dbf5e67
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-10-04 21:16:58 +0700

    feat(dependencies): Add DataStore for theme preferences
    Signed-off-by: gustyxpower <gustiadityamuzaky08@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,6 +108,9 @@ dependencies {
     // Navigation (Compose)
     implementation("androidx.navigation:navigation-compose:2.6.0")
 
+    // DataStore for theme preferences
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
